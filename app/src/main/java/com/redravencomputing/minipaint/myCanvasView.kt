6c1de18b3b11859e261ffcfc82b341469ce20ea8
Bruce <bruce@redravencomputing.com>
2021-08-29 23:08:27 -0600

    Add touch events

--- app/src/main/java/com/redravencomputing/minipaint/myCanvasView.kt
+++ app/src/main/java/com/redravencomputing/minipaint/myCanvasView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
@@ -29,6 +31,11 @@ class MyCanvasView(context: Context) : View(context) {
         strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
     }
 
+    private var path = Path()
+
+    private var motionTouchEventX: Float? = 0f
+    private var motionTouchEventY: Float? = 0f
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         if (::extraBitmap.isInitialized) extraBitmap.recycle() //Used to prevent memory leak by recycling the bitmap
@@ -40,5 +47,23 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    private fun touchStart() {}
+
+    private fun touchMove() {}
+
+    private fun touchUp() {}
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        motionTouchEventX = event?.x
+        motionTouchEventY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
     }
 }
