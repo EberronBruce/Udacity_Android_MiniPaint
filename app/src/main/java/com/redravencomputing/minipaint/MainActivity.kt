86160a0c8f5036aed87576aa06d8890654c31fd9
Bruce <bruce@redravencomputing.com>
2021-08-29 22:20:57 -0600

    Added canvas and bitmap and set myCanvas to setContent in MainActivity.kt

--- app/src/main/java/com/redravencomputing/minipaint/MainActivity.kt
+++ app/src/main/java/com/redravencomputing/minipaint/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.redravencomputing.minipaint
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            @Suppress("DEPRECATION")
+            myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        }
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
